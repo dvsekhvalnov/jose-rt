6522e7341966e06b98d24bc4ddf28601ddb6a1c0
DV <dvsekhvalnov@gmail.com>
2014-08-17 11:29:29 +0400

    compact support

---
+++ JoseRT/compact/Compact.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace JoseRT.Compact
+{
+    public sealed class Compact
+    {
+        public static Part[] Parse(string token)
+        {
+            string[] parts = token.Split('.');
+
+            var result = new Part[parts.Length];
+
+            for (var i = 0; i < parts.Length; i++)
+            {
+                result[i]=new Part(Base64Url.Decode(parts[i]));
+            }
+
+            return result;
+        }
+
+        public static string Serialize(params Part[] parts)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var part in parts)
+            {
+                builder.Append(Base64Url.Encode(part.Bytes)).Append(".");
+            }
+
+            builder.Remove(builder.Length - 1, 1);
+
+            return builder.ToString();
+        }
+    }
+
+    public sealed class Part
+    {
+        private byte[] _bytes;
+
+        public Part([ReadOnlyArray] byte[] bytes)
+        {
+            _bytes = bytes;
+        }       
+
+        public byte[] Bytes
+        {
+            get { return _bytes; }
+        }
+    }
+}
---
+++ Win8Tests/Base64UrlTest.cs
@@ -0,0 +1,31 @@
+﻿using JoseRT;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Win8Tests
+{
+    [TestClass]
+    public class Base64UrlTest
+    {
+        [TestMethod]
+        public void Encode()
+        {
+            //when
+            var test = Base64Url.Encode(new byte[] { 72, 101, 108, 108, 111, 32, 66, 97, 115, 101, 54, 52, 85, 114, 108, 32, 101, 110, 99, 111, 100, 105, 110, 103, 33 });
+
+            //then
+            Assert.AreEqual(test, "SGVsbG8gQmFzZTY0VXJsIGVuY29kaW5nIQ");
+        }
+
+        [TestMethod]
+        public void Decode()
+        {
+            //when
+            var test = Base64Url.Decode("SGVsbG8gQmFzZTY0VXJsIGVuY29kaW5nIQ");
+
+            //then
+            CollectionAssert.AreEqual(test, new byte[] { 72, 101, 108, 108, 111, 32, 66, 97, 115, 101, 54, 52, 85, 114, 108, 32, 101, 110, 99, 111, 100, 105, 110, 103, 33 });
+        }
+
+ 
+    }
+}
